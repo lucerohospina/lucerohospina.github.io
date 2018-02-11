e5dba5ae64bf4fe1544830f63ce5856b50389bc1
lucerohospina <lucerojhp@gmail.com>
2018-02-11 16:14:44 -0500

    adding folders, files and initial page model

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $('#about-me').click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({ 
+      scrollTop: $('#about-me-target').offset().top-98
+    }, 1000);
+  });
+  $('#projects').click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({ 
+      scrollTop: $('#projects-target').offset().top-98
+    }, 1000);
+  });
+  $('#contact-me').click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({ 
+      scrollTop: $('#contact-me-target').offset().top-98
+    }, 1000);
+  });
+
+  $('#eng-languaje').click(function(){
+    window.location.href = 'index.html'
+  })
+
+  $('#esp-language').click(function(){
+    window.location.href = 'index-esp.html'
+  })
+});
